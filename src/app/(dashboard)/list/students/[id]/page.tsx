b004ac7269f91752ac065e4eb020978e17df036e
Hemant Raj <coder.hemantraj05@gmail.com>
2025-03-24 11:20:47 +0530

    Remove unused announcement and event list pages; refactor assignment, exam, lesson, and result list pages to utilize Prisma for data fetching and pagination, enhancing query handling and user experience.

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -7,10 +7,13 @@ import Link from "next/link";
 const SingleStudentPage = () => {
   return (
     <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
+      
       {/* LEFT */}
       <div className="w-full xl:w-2/3">
+
         {/* TOP */}
         <div className="flex flex-col lg:flex-row gap-4">
+          
           {/* USER INFO CARD */}
           <div className="bg-lamaSky py-6 px-4 rounded-md flex-1 flex gap-4">
             <div className="w-1/3">
@@ -47,8 +50,10 @@ const SingleStudentPage = () => {
               </div>
             </div>
           </div>
+          
           {/* SMALL CARDS */}
           <div className="flex-1 flex gap-4 justify-between flex-wrap">
+            
             {/* CARD */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
@@ -63,6 +68,7 @@ const SingleStudentPage = () => {
                 <span className="text-sm text-gray-400">Attendance</span>
               </div>
             </div>
+            
             {/* CARD */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
@@ -77,6 +83,7 @@ const SingleStudentPage = () => {
                 <span className="text-sm text-gray-400">Grade</span>
               </div>
             </div>
+            
             {/* CARD */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
@@ -91,6 +98,7 @@ const SingleStudentPage = () => {
                 <span className="text-sm text-gray-400">Lessons</span>
               </div>
             </div>
+            
             {/* CARD */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
@@ -107,18 +115,23 @@ const SingleStudentPage = () => {
             </div>
           </div>
         </div>
+        
         {/* BOTTOM */}
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
           <h1>Student&apos;s Schedule</h1>
           <BigCalendar />
         </div>
       </div>
+      
       {/* RIGHT */}
       <div className="w-full xl:w-1/3 flex flex-col gap-4">
         <div className="bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Shortcuts</h1>
           <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
+            <Link
+              className="p-3 rounded-md bg-lamaSkyLight"
+              href={`/list/lessons?classId=${2}`}
+            >
               Student&apos;s Lessons
             </Link>
             <Link
@@ -127,13 +140,13 @@ const SingleStudentPage = () => {
             >
               Student&apos;s Teachers
             </Link>
-            <Link className="p-3 rounded-md bg-pink-50" href="/">
+            <Link className="p-3 rounded-md bg-pink-50" href={`/list/exams?classId=${2}`}>
               Student&apos;s Exams
             </Link>
-            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
+            <Link className="p-3 rounded-md bg-lamaSkyLight" href={`/list/assignments?classId=${2}`}>
               Student&apos;s Assignments
             </Link>
-            <Link className="p-3 rounded-md bg-lamaYellowLight" href="/">
+            <Link className="p-3 rounded-md bg-lamaYellowLight" href={`/list/results?studentId=${"student2"}`}>
               Student&apos;s Results
             </Link>
           </div>
